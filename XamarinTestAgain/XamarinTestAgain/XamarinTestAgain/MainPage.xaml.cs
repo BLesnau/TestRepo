ebef5843df30ed00fedfa64b9bc0f401c2946c8b
Brett Lesnau <lesnaubr@gmail.com>
2017-09-08 07:58:37 -0400

    Test

---
+++ XamarinTestAgain/XamarinTestAgain/XamarinTestAgain/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinTestAgain
+{
+   public partial class MainPage : ContentPage
+   {
+      public string TestText { get; set; }
+      public List<string> Items1 { get; set; }
+      public List<string> Items2 { get; set; }
+      public List<string> Items3 { get; set; }
+
+      public MainPage()
+      {
+         TestText = "Yup this is Brett test!";
+         Items1 = new List<string>() { "Hey", "Brett Lesnau" };
+         Items2 = new List<string>() { "You" };
+         Items3 = new List<string>() { "Uhhh", "Nope", "Gee Whiz" };
+         this.BindingContext = this;
+         InitializeComponent();
+      }
+   }
+}
